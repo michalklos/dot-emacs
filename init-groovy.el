36b5c120ae6a30b8c0b02d693ba54ae980377130
Michal Klos <mklos@turbine.com>
2012-11-15 14:33:34 -0500

    Groovy [ADD]: added groovy and gradle support

---
+++ init-groovy.el
@@ -0,0 +1,36 @@
+
+;;; turn on syntax highlighting
+(global-font-lock-mode 1)
+
+;;; use groovy-mode when file ends in .groovy or has #!/bin/groovy at start
+(autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
+(add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
+(add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
+
+;;; make Groovy mode electric by default.
+(add-hook 'groovy-mode-hook
+          '(lambda ()
+             (require 'groovy-electric)
+             (groovy-electric-mode)))
+
+
+;; Get the Groovy support for Emacs from http://svn.codehaus.org/groovy/trunk/groovy/ide/emacs
+;; Symlink the downloaded Groovy support into your .emacs.d folder as "groovy"
+
+;;; use groovy-mode when file ends in .groovy or has #!/bin/groovy at start
+(add-to-list 'load-path "~/.emacs.d/groovy")
+(autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
+;; For some reason, these recommendations don't seem to work with Aquamacs
+(add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
+(add-to-list 'auto-mode-alist '("\.gradle$" . groovy-mode))
+;; This does work with Aquamacs
+(add-to-list 'auto-mode-alist (cons "\\.gradle\\'" 'groovy-mode))
+(add-to-list 'auto-mode-alist (cons "\\.groovy\\'" 'groovy-mode))
+;; This _might_ not work with Aquamacs (not sure what value it offers)
+(add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
+(add-to-list 'interpreter-mode-alist '("gradle" . groovy-mode))
+;;; make Groovy mode electric by default.
+(add-hook 'groovy-mode-hook
+          '(lambda ()
+             (require 'groovy-electric)
+             (groovy-electric-mode)))
